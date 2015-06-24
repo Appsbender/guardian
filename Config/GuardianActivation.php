5b99fb2d7ab7b05e2b94b575a588144fb4aeadbe
Rachman Chavik <rchavik@xintesa.com>
2015-06-24 15:36:21 +0700

    Add plugin activation class

---
+++ Config/GuardianActivation.php
@@ -0,0 +1,48 @@
+<?php
+
+class GuardianActivation {
+
+	public function beforeActivation(&$controller) {
+		return true;
+	}
+
+	public function onActivation(&$controller) {
+		$guardKey = Configure::read('Guardian.guardKey');
+		if (empty($guardKey)) {
+			$guardKey = sha1(uniqid());
+			$Setting = ClassRegistry::init('Settings.Setting');
+			$setting = $Setting->write('Guardian.guardKey', $guardKey, array(
+				'title' => 'Guard Key',
+				'description' => 'Append this value in the admin URL are at the start of each session',
+				'editable' => true,
+				'input_type' => 'text',
+			));
+			$controller->Session->write('Guardian.guardKey', $guardKey);
+			$controller->Session->setFlash(sprintf(
+				'Admin area has been protected with URL: %s',
+				Router::url(Hash::merge(
+					Configure::read('Croogo.dashboardUrl'),
+					array('?' => $guardKey)
+				), true)
+			), 'flash', array('class' => 'success'));
+
+			$controller->redirect(array(
+				'admin' => true,
+				'plugin' => 'settings',
+				'controller' => 'settings',
+				'action' => 'prefix',
+				'Guardian',
+			));
+		}
+	}
+
+	public function beforeDeactivation(&$controller) {
+		return true;
+	}
+
+	public function onDeactivation(&$controller) {
+		$Setting = ClassRegistry::init('Settings.Setting');
+		$Setting->deleteKey('Guardian.guardKey');
+		$controller->Session->delete('Guardian');
+	}
+}
